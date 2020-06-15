1c9070a52854b8313343f04b1d682436a4830351
codingbeginner19 <60525942+codingbeginner19@users.noreply.github.com>
2020-06-15 15:39:25 -0400

    Add files via upload

--- main.js
+++ main.js
@@ -1,26 +1,28 @@
-$(function () {
+function main() {
     $(".fa-gamepad").on("click", function (event) {
         $("#landingPage").addClass("hide");
         $("#searchPage").removeClass("hide");
-    })
+    });
     $("form").on("submit", function (event) {
-        event.preventDefault()
-        let search = $("#search").val()
+        event.preventDefault();
+        let search = $("#search").val();
         let url = `https://api.rawg.io/api/games?search=${search}`
         fetch(url)
             .then(response => response.json())
             .then(data => {
-                let view = $("#dataView")
-
+                let view = $("#dataView");
+                view.empty()
+                if (data.count === 0) {
+                    view.append("No Results");
+                    return;
+                };
                 $.each(data.results, function (index, game) {
-                    let str = `<div class = "card Game">Name: ${game.name}, <br> Release Date:${game.released},<br> Rating:${game.rating},<br><img src = "${game.background_image}"></img></div>`
-                    view.append(str)
-                })
-                console.log(data.results)
+                    let str = `<div class = "card Game">Name: ${game.name}, <br> Release Date:${game.released},<br> Rating:${game.rating},<br><img src = "${game.background_image}"></img></div>`;
+                    view.append(str);
 
+                });
+                console.log(data.results);
             });
-
-    })
-
-
-});
+    });
+};
+$(main());
